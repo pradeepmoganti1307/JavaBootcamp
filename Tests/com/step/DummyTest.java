90b4a40598e256c79093716fb4831ee97ddd4cb9
pradeepmoganti1307 <183721611+pradeepmoganti1307@users.noreply.github.com>
2025-05-19 10:22:54 +0530

    added dummy test

---
+++ Tests/com/step/DummyTest.java
@@ -0,0 +1,12 @@
+package com.step;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DummyTest {
+    @Test
+    void firstTest() {
+        assert(true);
+    }
+}
